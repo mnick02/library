a0726f785c5c200f953ba7aa9540ba90d2531ab1
mnick02 <nokiamn3@gmail.com>
2025-03-09 13:41:04 -0700

    Create addBookToLibrary func and id to Book func
    Created addBookToLibrary func which will call the
    Book func and then push that to the myLibrary
    array. Also added id using crypto.randomUUID to
    Book func to give unique id to each book

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ function Book (title, author, pages, read) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
 
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+
+function addBookToLibrary (title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
+    console.log(newBook.id)
 }
