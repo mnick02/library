3ead1d8a27c20a68b58cf16fbc152631aa76b3f4
mnick02 <nokiamn3@gmail.com>
2025-03-10 12:24:27 -0700

    Change from table-body to table-row in displayBook
    Changed the array items being added to a table-row
    instead of a table-body in the display book func

--- script.js
+++ script.js
@@ -19,49 +19,15 @@ function displayBook() {
     const tblContainer = document.querySelector("#tbl-container");
     tblContainer.innerHTML = '';
 
-    /*myLibrary.forEach(item => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        
-
-        card.innerHTML = `
-            <h2>${item.title}</h2>
-            <p>${item.author}</p>
-            <p>${item.pages}</p>
-            <p>${item.read}</p>
-        `;
-
-        container.appendChild(card);
-    });*/
-    
-    /*myLibrary.forEach(item => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        card.innerHTML += `
-            <table>
-                <tbody>
-                    <th>${item.title}</th>
-                    <td>${item.author}</td>
-                    <td>${item.pages}</td>
-                    <td>${item.read}</td>
-                </tbody>
-            </table>
-        `;
-        container.appendChild(card);
-    });*/
-
     myLibrary.forEach(item => {
-        const tb = document.createElement("tbody");
-
-
-        tb.innerHTML = `
+        const tableRow = document.createElement("tr");
+        tableRow.innerHTML = `
             <th>${item.title}</th>
             <td>${item.author}</td>
             <td>${item.pages}</td>
             <td>${item.read}</td>
         `;
-        tblContainer.appendChild(tb);   
+        tblContainer.appendChild(tableRow);   
     
     });
     
