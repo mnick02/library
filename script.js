db9177f94aaa34bd5907b048b342422e7481263e
mnick02 <nokiamn3@gmail.com>
2025-03-10 12:18:53 -0700

    Create displayBook func
    Create displayBook func that iterates through each
    item in the myLibrary array, adding that data to a
    table body that is appended to the table container
    ; addBookToLibrary func now calls this func at now

--- script.js
+++ script.js
@@ -12,12 +12,66 @@ function Book (title, author, pages, read) {
     this.read = read;
 }
 
+const container = document.querySelector("#container");
+
+function displayBook() {
+    container.innerHTML = '';
+    const tblContainer = document.querySelector("#tbl-container");
+    tblContainer.innerHTML = '';
+
+    /*myLibrary.forEach(item => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        
+
+        card.innerHTML = `
+            <h2>${item.title}</h2>
+            <p>${item.author}</p>
+            <p>${item.pages}</p>
+            <p>${item.read}</p>
+        `;
+
+        container.appendChild(card);
+    });*/
+    
+    /*myLibrary.forEach(item => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML += `
+            <table>
+                <tbody>
+                    <th>${item.title}</th>
+                    <td>${item.author}</td>
+                    <td>${item.pages}</td>
+                    <td>${item.read}</td>
+                </tbody>
+            </table>
+        `;
+        container.appendChild(card);
+    });*/
+
+    myLibrary.forEach(item => {
+        const tb = document.createElement("tbody");
+
+
+        tb.innerHTML = `
+            <th>${item.title}</th>
+            <td>${item.author}</td>
+            <td>${item.pages}</td>
+            <td>${item.read}</td>
+        `;
+        tblContainer.appendChild(tb);   
+    
+    });
+    
+}
+
+
 
 function addBookToLibrary (title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
-
     myLibrary.push(newBook);
-    console.log(newBook);
     console.log(myLibrary);
-    console.log(newBook.id)
+    displayBook();
 }
